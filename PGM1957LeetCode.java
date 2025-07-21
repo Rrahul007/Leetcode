3706b9be400588daeb76630f20438e762727dd97
Rahul Adke <rsadke007@gmail.com>
2025-07-22 01:31:02 +0530

    Create PGM1957LeetCode.java

---
+++ PGM1957LeetCode.java
@@ -0,0 +1,12 @@
+//Delete Characters to Make Fancy String LeetCode Solution in Java
+class Solution {
+    public String makeFancyString(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray())
+        {
+            if(sb.length() < 2 || sb.charAt(sb.length() - 1) != c || sb.charAt(sb.length() - 2) != c)
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
